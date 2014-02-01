f2d979099974ebf93dec38ea90799632e3b75fbf
James Marquardt <jamarq@jamarq-1015PX.(none)>
2014-01-31 19:38:03 -0800

    Adds two functions to split input data into 10 folds to Util, unable to push new .py file

--- Util.py
+++ Util.py
@@ -1,4 +1,7 @@
-import os
+from shutil import copy
+from sklearn.cross_validation import KFold
+from CSVMaker import csvLineMaker
+import os, csv
 
 #Save textual content to a file
 #Input: file path and textual content
@@ -17,3 +20,51 @@ def readFile(filePath):
 def saveFolder(folderPath):  
     if not os.path.exists(folderPath):
         os.makedirs(folderPath) 
+
+#Splits an input .csv by 10 folds by line into .csv's 
+def tenFoldSplitCSV(src_csv, dst_path):
+	with open(src_csv, 'rb') as csvfile:
+		if not os.path.exists(dst_path):
+			os.makedirs(dst_path)
+		length = 0
+		lines = []
+		reader = csv.reader(csvfile, delimiter=',')
+		for row in reader:
+			lines.append(row)
+			length+=1
+		kf = KFold(length, n_folds = 10)
+		dir_iter = 1
+		for train, test in kf:
+			foldcsv = "Train\n"			
+			for train_index in train:
+				foldcsv = foldcsv + csvLineMaker(lines[train_index]) + "\n"
+			foldcsv = foldcsv + "Test\n"
+			for test_index in test:
+				foldcsv = foldcsv + csvLineMaker(lines[train_index]) + "\n"
+			saveFile(dst_path + "fold" + str(dir_iter) + ".csv", foldcsv)
+			dir_iter+=1
+
+#Split data in directory into 10 folds.
+###Not an efficient solution###
+def tenFoldSplitDir(src_path, dst_path):
+	files = getFilesFromFolder(src_path)
+	kf = KFold(len(files), n_folds = 10)
+
+	dir_iter = 1
+
+	for train, test in kf:
+		
+		train_dest = dst_path + "train-set" + str(dir_iter) + "/"
+		test_dest = dst_path + "test-set" + str(dir_iter) + "/"
+
+		if not os.path.exists(train_dest):
+			os.makedirs(train_dest)
+		if not os.path.exists(test_dest):
+			os.makedirs(test_dest)
+
+		for train_index in train:
+			copy(src_path + files[train_index], train_dest)
+		for test_index in test:
+			copy(src_path + files[test_index], test_dest)
+
+		dir_iter+=1
