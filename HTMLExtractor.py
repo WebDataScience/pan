a319325e7e12d0694e9d93012c7129b752dd1375
James Marquardt <jamarq@jamarq-1015PX.(none)>
2014-02-22 10:11:05 -0800

    HTML tags extracted by parsing DOM. More accurate, less efficient

--- HTMLExtractor.py
+++ HTMLExtractor.py
@@ -1,5 +1,6 @@
 from os import listdir
 from os.path import isfile, join
+from bs4 import BeautifulSoup
 import re
 
 def countTags(src):
@@ -8,25 +9,17 @@ def countTags(src):
 	only_files = [ f for f in listdir(src) if isfile(join(src,f)) ]
 	for entry in only_files:
 		doc_arr = dict()
-		
-		read_file = open(src+"/"+entry, "rb")
-		contents = read_file.read()
+		soup = BeautifulSoup(open(src+"/"+entry,"rb"))
 
-		links = re.compile('<a href')
-		images = re.compile('<img')
-		bold = re.compile('<b')
-		italic = re.compile('<i')
-		lists = re.compile('((<ul)|(<ol))')
-		
-		doc_arr['links'] = len(links.findall(contents))
-		doc_arr['images'] = len(images.findall(contents))
-		doc_arr['bold'] = len(bold.findall(contents))
-		doc_arr['italic'] = len(italic.findall(contents))
-		doc_arr['lists'] = len(lists.findall(contents))
+		doc_arr["links"] = len(soup.find_all('a'))
+		doc_arr["image"] = len(soup.find_all('img'))
+		doc_arr["bold"] = len(soup.find_all('b'))
+		doc_arr["italic"] = len(soup.find_all('i'))
+		doc_arr["lists"] = len(soup.find_all('ul')) + len(soup.find_all('ol'))
 
 		ret_arr[entry.split("_")[0]]=doc_arr
-		print ret_arr
-	
-	return ret_arr
 
-countTags("/home/jamarq/PAN/Data/PAN14/PAN14/pan14-author-profiling-training-corpus-blogs-2014-02-10/en")
+	print ret_arr
+	return ret_arr	
+
+#countTags("/home/jamarq/PAN/Data/PAN14/PAN14/pan14-author-profiling-training-corpus-blogs-2014-02-10/en")
